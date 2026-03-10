bbb1576cf3e5b8dd828aa33fd38d27ba4d79a8e2
Lonhop <egore_a@mail.ru>
2026-03-11 00:29:39 +0300

    Добавлен класс ThreadPool. Возможность добавления задач(с приоритетом, возвращаемым значением и т д), проверки состояния, изменение потока и очистка очереди

--- project/include/core/thread_pool.h
+++ project/include/core/thread_pool.h
@@ -38,6 +38,56 @@ namespace redis::core {
         size_t total_tasks_failed = 0;  // всего задач с ошибками
         std::chrono::milliseconds avg_wait_time{0}; // среднее время ожидания
     };
+
+    class ThreadPool {
+    public:
+        //  Пул с числом потоков
+        explicit ThreadPool(size_t numThreads);
+        // Создание пула с конфигурацией
+        explicit ThreadPool(const ThreadPoolConfig& config);
+
+        // Запрет Копирования
+        ThreadPool(const ThreadPool&) = delete;
+        ThreadPool& operator=(const ThreadPool&) = delete;
+        // Перемещение
+        ThreadPool(ThreadPool&& other) noexcept;
+        ThreadPool& operator=(ThreadPool&& other) noexcept;
+        // Деструктор
+        ~ThreadPool();
+
+        // Добавление задачи без возвращаемого значения
+        void enqueue(std::function<void()> task);
+        // Добавление задачи с приоритетом
+        void enqueue(std::function<void()> task, TaskPriority priority);
+        // Добавление задачи с возвращаемым значением
+        template<typename F, typename... Args>
+        auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>>;
+        // Добавление задачи с возвращаемым значением и приоритетом
+        template<typename F, typename... Args>
+        auto enqueue(TaskPriority priority, F&& f, Args&&... args) -> std::future<typename std::invoke_result_t<F, Args...>>;
+
+
+        // Динамическое изменение числа потоков
+        bool resize(size_t new_size);
+        // Приостановка и возобновление обработки
+        void pause();
+        void resume();
+        // Очистка очереди задач
+        std::vector<std::function<void()>> clear();
+
+        void waitAll(); // Ожидание завершения всех задач в очереди
+        bool waitAllFor(std::chrono::milliseconds timeout); // Ожидание завершения всех задач с таймаутом
+
+        // Проверка состояния
+        bool isRunning() const noexcept { return !stop_.load(); }
+        bool isPaused() const noexcept { return paused_.load(); }
+        size_t pendingTasks() const noexcept { return tasks_.size(); }
+        size_t activeThreads() const noexcept;
+        size_t threadCount() const noexcept { return workers_.size(); }
+        // Получение статистики
+        ThreadPoolStats getStats() const;
+
+    };
 }
 
 #endif
