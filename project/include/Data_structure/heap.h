c090f872c68c82b86d494cfe51e8e4032aabe639
Lonhop <egore_a@mail.ru>
2026-03-10 12:33:29 +0300

    Добавления класса Heap с конструкторами и функциями для TTL(time to live)

--- project/include/Data_structure/heap.h
+++ project/include/Data_structure/heap.h
@@ -24,6 +24,64 @@ namespace redis::data_structures {
         bool operator>=(const HeapItem& other) const {return val >= other.value; }
     };
 
+    class Heap {
+    public:
+        Heap() = default;
+        ~Heap() - default;
+
+        // Запрепт копирования
+        Heap(const Heap&) = delete;
+        Heap& operator=(const Heap&) = delete;
+
+        // Перемещение
+        Heap(Heap&& other) noexcept;
+        Heap& operator=(Heap&& other) noexcept;
+
+
+        // Операции
+        void push(uint64_t val, size_t* ref);
+        void update(size_t pos, uint64_t new_val);
+        void erase(size_t pos);
+
+        // Доступ к 1 элементу
+        const HeapItem& top() const;
+        HeapItem& top();
+
+        // Проверка на пустоту, получение размера и очищение
+        bool empty() const noexcept { return heap_.empty(); }
+        size_t size() const noexcept { return heap_.size(); }
+        void clear() noexcept { return heap_.clear(); }
+
+        // Поиск
+        bool contains(size_t pos) const noexcept;
+        const HeapItem& at(size_t size) const;
+        HeapItem& at(size_t pos);
+
+        // Итераторы
+        using iterator = std::vector<HeapItem>::iterator;
+        using constiterator = std::vector<HeapItem>::const_iterator;
+
+        // Функции доступа итераторов
+        iterator begin() noexcept { return heap_.begin(); }
+        iterator end() noexcept { return heap_.end(); }
+        const_iterator begin() const noexcept { return heap_.begin(); }
+        const_iterator end() const noexcept { return heap_.end(); }
+        const_iterator cbegin() const noexcept { return heap_.cbegin(); }
+        const_iterator cend() const noexcept { return heap_.cend(); }
+
+    private:
+        std::vector<HeapItem> heap_;
+
+        static size_t parent(size_t t) noexcept { return (t - 1) / 2; }
+        static size_t left(size_t t) noexcept { return t * 2 + 1; }
+        static size_t right(size_t t) noexcept { return t * 2 + 2; }
+
+        void up(size_t pos);
+        void down(size_t pos);
+        void spaw_items(size_t i, size_t j);
+        void validate_pos(size_t pos) const;
+
+    };
 }
 
 #endif //REDIS_SERVER_HEAP_H
